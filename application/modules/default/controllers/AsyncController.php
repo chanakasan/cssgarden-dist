e47b33b5b2828ef09ed305d222a20a174ccf26af
chanakasandaruwan <chanakasan@gmail.com>
2012-12-22 18:17:17 +0530

    Fix: entry form ajax call for selecting cities

--- application/modules/default/controllers/AsyncController.php
+++ application/modules/default/controllers/AsyncController.php
@@ -13,6 +13,7 @@ class AsyncController extends Zend_Controller_Action
     */
     private $_session;
 
+
     public function init()
     {
         $this->_helper->viewRenderer->setNoRender();
@@ -21,25 +22,46 @@ class AsyncController extends Zend_Controller_Action
 
     public function preDisptch()
     {
-        //$this->_session = new Zend_Session_Namespace('default');
+        $this->_session = new Zend_Session_Namespace('default');
 
         if(!$this->_session->view)
                 $this->_session->view = $this->view;
-
     }
 
     public function loadcityAction()
     {
+        $result[1] = array(
+                    array("id" => "1", "area_id"=>"1", "name" => "Colombo 1"),
+                    array("id" => "2", "area_id"=>"1", "name" => "Colombo 2"),
+                    array("id" => "3", "area_id"=>"1", "name" => "Colombo 3"),
+                );
+        $result[2] = array(
+                    array("id" => "4", "area_id"=>"2", "name" => "Abepussa"),
+                    array("id" => "5", "area_id"=>"2", "name" => "Biyagama"),
+                    array("id" => "6", "area_id"=>"2", "name" => "Dalugama")
+                );
+        $nocity = array("id" => "0" , "name" => "Select an area");
+
         if($this->getRequest()->isPost())
         {
-            echo "load city";
+            $postData = $this->_request->getPost();
+            if(isset($postData['sel_area']))
+            {
+                $id = $postData['sel_area'];
+            }
+            if($id > 0 && $id < 3)
+            {
+                echo Zend_Json::encode($result[$id]);
+            }
+            else
+                echo Zend_Json::encode(array($nocity));
         }
     }   
     
     
     public function getsecAction()
     {      
-        echo Zend_Json_Encoder::encode($this->getSections());
+       
     }
 
     
