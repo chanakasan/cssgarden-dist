a615a4f26c4e603bda5ce7d42c639dcbc847b4e4
chanakasandaruwan <chanakasan@gmail.com>
2012-12-23 19:01:48 +0530

    Fix: loading area and city select lists using ajax. works ok! :)

--- application/modules/default/controllers/AsyncController.php
+++ application/modules/default/controllers/AsyncController.php
@@ -34,17 +34,6 @@ class AsyncController extends Zend_Controller_Action
 
     public function loadcityAction()
     {
-//        $result[1] = array(
-//                    array("id" => "1", "area_id"=>"1", "name" => "Colombo 1"),
-//                    array("id" => "2", "area_id"=>"1", "name" => "Colombo 2"),
-//                    array("id" => "3", "area_id"=>"1", "name" => "Colombo 3"),
-//                );
-//        $result[2] = array(
-//                    array("id" => "4", "area_id"=>"2", "name" => "Abepussa"),
-//                    array("id" => "5", "area_id"=>"2", "name" => "Biyagama"),
-//                    array("id" => "6", "area_id"=>"2", "name" => "Dalugama")
-//                );
-
         $nocity = array("id" => "0" , "name" => "Select an area");
 
         if($this->getRequest()->isPost())
@@ -52,7 +41,7 @@ class AsyncController extends Zend_Controller_Action
             $postData = $this->_request->getPost();
             if(isset($postData['sel_area']))
             {
-                $id = $postData['sel_area'];                
+                $id = $postData['sel_area'];
             }
             if($id > 0)
             {
@@ -60,9 +49,17 @@ class AsyncController extends Zend_Controller_Action
                 $em = $this->_doctrineContainer->getEntityManager();
                 $query = $em->createQuery("SELECT a FROM App\Entity\Area a WHERE a.id = :id");
                 $query->setParameter("id", $id);
-                $result = $query->getResult();
-                // send result to browser
-                echo Zend_Json::encode($result);
+                $area = $query->getResult();
+                if(!empty($area))// send result to browser
+                {
+                    $cities = $area[0]->cities;
+                    $list = array();
+                    foreach($cities as $city)
+                    {
+                        $list[] = $city->toArray();                        
+                    }                    
+                    echo Zend_Json::encode($list);
+                }
             }
             else
                 echo Zend_Json::encode(array($nocity));
--- library/App/Entity/City.php
+++ library/App/Entity/City.php
@@ -87,6 +87,17 @@ class City
        return get_object_vars($this);
     }
 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
     public function getName()
     {
         return $this->name;
